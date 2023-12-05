66806393f533c79aed781fbcba911fabff7f19d7
mikefromru <googlockgood@gmail.com>
2023-12-05 23:40:53 +0300

    Added FSM

--- handlers/client.py
+++ handlers/client.py
@@ -1,14 +1,91 @@
 import asyncio
-from aiogram import Router
+from aiogram import Router, html
 from aiogram.filters import Command 
 from aiogram.utils.markdown import hbold
+from aiogram.enums import ParseMode
 from aiogram.types import Message
-from aiogram import Bot
+
+
+from typing import Any, Dict
+
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 
 client = Router()
 
-@client.message(Command('delivery'))
-async def cmd_start(message: Message):
-    await message.reply('Delivery func')
+class Form(StatesGroup):
 
+    description = State()
+    weight = State()
+    size = State()
+    from_adress = State()
+    to_adress = State()
+    method_pay = State()
+
+
+@client.message(Command('delivery'))
+async def cmd_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.description)
+    await message.reply('Напишите описание груза')
+
+
+@client.message(Form.description)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(description=message.text)
+    await state.set_state(Form.weight)
+    await message.answer('ty[e the weight]')
+
+
+@client.message(Form.weight)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(weight=message.text)
+    await state.set_state(Form.size)
+    await message.answer('type the size')
+
+
+@client.message(Form.size)
+async def process_name(message: Message, state: FSMContext) -> None:
+    data = await state.update_data(size=message.text)
+    await state.set_state(Form.from_adress)
+    await show_summary(message=message, data=data)
+
+
+@client.message(Form.from_adress)
+async def process_name(message: Message, state: FSMContext) -> None:
+    data = await state.update_data(from_adress=message.text)
+    await state.set_state(Form.to_adress)
+    await show_summary(message=message, data=data)
+
+
+@client.message(Form.to_adress)
+async def process_name(message: Message, state: FSMContext) -> None:
+    data = await state.update_data(to_adress=message.text)
+    await state.set_state(Form.method_pay)
+    await show_summary(message=message, data=data)
+
+
+@client.message(Form.method_pay)
+async def process_name(message: Message, state: FSMContext) -> None:
+    data = await state.update_data(method_py=message.text)
+    await state.clear()
+    await show_summary(message=message, data=data)
+
+
+async def show_summary(message: Message, data: Dict[str, Any], positive: bool = True) -> None:
+    description = data.get('description')
+    weight = data.get('weight')
+    size = data.get('size')
+    from_adress = data.get('from_adress')
+    to_adress = data.get('to_adress')
+    method_pay = data.get('method_pay')
+
+    text = f'Description is {html.bold(description)}\n'
+    text += f'Weight is {html.bold(weight)}\n'
+    text += f'Size is {html.bold(size)}\n'
+    text += f'Delivery from {html.bold(from_adress)}\n'
+    text += f'Delivery to {html.bold(to_adress)}\n'
+    text += f'Method to pay is {html.bold(method_pay)}'
+
+    await message.answer(text=text)
+   
