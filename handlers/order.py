30c4a975271de0569a4d0f4858bb88337c4bd5ba
mikefromru <googlockgood@gmail.com>
2023-12-06 19:21:37 +0300

    Added a State FormComplaint

--- handlers/order.py
+++ handlers/order.py
@@ -1,9 +1,6 @@
-import asyncio
 import logging
 from aiogram import Router, html, F
 from aiogram.filters import Command 
-from aiogram.utils.markdown import hbold
-from aiogram.enums import ParseMode
 from aiogram.types import Message
 from typing import Any, Dict
 from aiogram.fsm.context import FSMContext
@@ -17,7 +14,7 @@ from aiogram.types import (
 
 import fpdf
 
-client = Router()
+order_router = Router()
 
 class Form(StatesGroup):
 
@@ -29,24 +26,24 @@ class Form(StatesGroup):
     method_pay = State()
 
 
-@client.message(Command('delivery'))
-async def cmd_start(message: Message, state: FSMContext) -> None:
+@order_router.message(Command('delivery'))
+async def cmd_delivery(message: Message, state: FSMContext) -> None:
     await state.set_state(Form.description)
-    await message.answer(text='Напишите описание груза?',
+    await message.answer(text='Напишите описание груза',
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[
                 [
-                    KeyboardButton(text="Cancel"),
+                    KeyboardButton(text="Отменить"),
                 ]
             ],
-            resize_keyboard=True, one_time_keyboard=False,
+            resize_keyboard=True,
         ),
 
     )
 
 
-@client.message(Command("cancel"))
-@client.message(F.text.casefold() == "cancel")
+@order_router.message(Command("cancel"))
+@order_router.message(F.text.casefold() == "отменить")
 async def cancel_handler(message: Message, state: FSMContext) -> None:
     """
     Allow user to cancel any action
@@ -58,48 +55,48 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     logging.info("Cancelling state %r", current_state)
     await state.clear()
     await message.answer(
-        "Cancelled.",
+        'Отмена.',
         reply_markup=ReplyKeyboardRemove(),
     )
 
 
-@client.message(Form.description)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.description)
+async def process_description(message: Message, state: FSMContext) -> None:
     await state.update_data(description=message.text)
     await state.set_state(Form.weight)
-    await message.answer('ty[e the weight]')
+    await message.answer('Вес груза')
 
 
-@client.message(Form.weight)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.weight)
+async def process_weight(message: Message, state: FSMContext) -> None:
     await state.update_data(weight=message.text)
     await state.set_state(Form.size)
-    await message.answer('type the size')
+    await message.answer('Габариты груза')
 
 
-@client.message(Form.size)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.size)
+async def process_size(message: Message, state: FSMContext) -> None:
     await state.update_data(size=message.text)
     await state.set_state(Form.from_address)
-    await message.answer('type the from address')
+    await message.answer('Точный адрес отправки')
 
 
-@client.message(Form.from_address)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.from_address)
+async def process_from_address(message: Message, state: FSMContext) -> None:
     data = await state.update_data(from_address=message.text)
     await state.set_state(Form.to_address)
-    await message.answer('type the to address')
+    await message.answer('Точный адрес получателя')
 
 
-@client.message(Form.to_address)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.to_address)
+async def process_to_address(message: Message, state: FSMContext) -> None:
     data = await state.update_data(to_address=message.text)
     await state.set_state(Form.method_pay)
-    await message.answer('type the method pay')
+    await message.answer('Способ оплаты')
 
 
-@client.message(Form.method_pay)
-async def process_name(message: Message, state: FSMContext) -> None:
+@order_router.message(Form.method_pay)
+async def process_method_pay(message: Message, state: FSMContext) -> None:
     data = await state.update_data(method_pay=message.text)
     await state.clear()
     await show_summary(message=message, data=data)
@@ -121,6 +118,7 @@ async def show_summary(message: Message, data: Dict[str, Any], positive: bool = 
     text += f'Method to pay is {html.bold(method_pay)}'
 
     await message.answer(text=text)
+    await message.answer(text='Данные сохранены в PDF файл Накладная.pdf')
     write_pdf(description, weight, size, from_address, to_address, method_pay)
 
 def write_pdf(description, weight, size, from_address, to_address, method_pay):
@@ -128,7 +126,6 @@ def write_pdf(description, weight, size, from_address, to_address, method_pay):
     pdf = fpdf.FPDF(format='letter') #pdf format
     pdf.add_page() #create new page
     path = os.path.abspath('handlers/OpenSans-Bold.ttf')
-    # pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
     pdf.add_font('OpenSans', '', path, uni=True)
     pdf.set_font('OpenSans', size=12) # font and textsize
     pdf.cell(200, 10, txt='Документ', ln=1, align='T')
@@ -139,16 +136,3 @@ def write_pdf(description, weight, size, from_address, to_address, method_pay):
     pdf.cell(200, 10, txt=f'Точный адрес получения - {to_address}', ln=2, align='L')
     pdf.cell(200, 10, txt=f'Способ оплаты - {method_pay}', ln=3, align='L')
     pdf.output('Накладная.pdf')
-
-
-# def write_pdf(description, weight, size, from_address, to_address):
-#     pdf = fpdf.FPDF(format='letter') #pdf format
-#     pdf.add_page() #create new page
-#     pdf.set_font("Arial", size=12) # font and textsize
-#     pdf.cell(200, 10, txt='Накладная', ln=1, align='R')
-#     pdf.cell(200, 10, txt=f'Описание груза\n{description}', ln=2, align='L')
-#     pdf.cell(200, 10, txt=f'Вес груза - {weight}', ln=2, align='L')
-#     pdf.cell(200, 10, txt=f'Габариты груза - {size}', ln=2, align='L')
-#     pdf.cell(200, 10, txt=f'Адрес отправки - {from_address}', ln=2, align='L')
-#     pdf.cell(200, 10, txt=f'Адрес получателя - {to_address}', ln=3, align='L')
-#     pdf.output('nacladnay.pdf')
