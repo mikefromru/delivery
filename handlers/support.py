4dd701148b548fa9a236410d9d9046d6d74471cd
mikefromru <googlockgood@gmail.com>
2023-12-08 16:10:21 +0300

    Working on support func

---
+++ handlers/support.py
@@ -0,0 +1,36 @@
+import logging
+from aiogram import Router, html, F, Bot
+from aiogram.filters import Command 
+from aiogram.types import Message
+from typing import Any, Dict
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.exceptions import TelegramAPIError
+
+# from bot.config_reader import config
+
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+import os
+
+
+support_router = Router()
+
+@support_router.message(Command('support'))
+async def cmd_manager(message: Message, bot: Bot) -> None:
+    await bot.send_message(chat_id=os.getenv('MANAGER'), text='Вызов в чат!') # send to manager
+    await message.answer(text='Задайте мне ваши вопросы',
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text='Закончить'),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+
+    )
